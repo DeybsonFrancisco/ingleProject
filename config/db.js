11ff50e020f0f0728eeb969cfec3f16fcc6e8591
DeybsonFrancisco <deybsona7x@gmail.com>
2020-03-19 15:17:39 -0300

    ajuste

--- config/db.js
+++ config/db.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://admin:admin@cluster0-wmbu5.mongodb.net/ingleProject
-", {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-});
+mongoose.connect(
+    "mongodb+srv://admin:admin@cluster0-wmbu5.mongodb.net/ingleProject",
+    {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    }
+);
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
